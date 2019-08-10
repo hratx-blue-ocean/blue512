c6660fa876f008db1aa251ec69c1f3356744f6a1
Gary Blaser <glblaser@gmail.com>
2019-08-10 14:37:17 -0500

    Cleaned up schema file and image

--- db/schema.sql
+++ db/schema.sql
@@ -1,108 +1,105 @@
-CREATE TABLE "Users" (
-    "ID" int   NOT NULL,
-    "Name" string   NOT NULL,
-    CONSTRAINT "pk_Users" PRIMARY KEY (
-        "ID"
-     )
+CREATE TABLE "users" (
+  "id" serial NOT NULL,
+  "name" varchar,
+  CONSTRAINT "pk_users" PRIMARY KEY (
+    "id"
+   )
 );
 
-CREATE TABLE "Sessions" (
-    "ID" int   NOT NULL,
-    "User_ID" int   NOT NULL,
-    "Token" string   NOT NULL,
-    CONSTRAINT "pk_Sessions" PRIMARY KEY (
-        "ID"
-     )
+CREATE TABLE "sessions" (
+  "id" serial NOT NULL,
+  "user_id" int,
+  "token" varchar,
+  CONSTRAINT "pk_sessions" PRIMARY KEY (
+    "id"
+   )
 );
 
-CREATE TABLE "Unvailable" (
-    "ID" int   NOT NULL,
-    "Time_Start" string   NOT NULL,
-    "Time_End" string   NOT NULL,
-    "User_ID" int   NOT NULL,
-    CONSTRAINT "pk_Unvailable" PRIMARY KEY (
-        "ID"
-     )
+CREATE TABLE "unvailable" (
+  "id" serial NOT NULL,
+  "time_Start" varchar,
+  "time_End" varchar,
+  "user_id" int,
+  CONSTRAINT "pk_unvailable" PRIMARY KEY (
+    "id"
+   )
 );
 
-CREATE TABLE "Experiences" (
-    "ID" int   NOT NULL,
-    "Name" string   NOT NULL,
-    "Source_API_ID" int   NOT NULL,
-    "Experience_API_ID" string   NOT NULL,
-    "Description" string   NOT NULL,
-    "URL" string   NOT NULL,
-    "Img_Large" string   NOT NULL,
-    "Img_Small" string   NOT NULL,
-    "Venue" string   NOT NULL,
-    "Location" string   NOT NULL,
-    "Time_Start" string   NOT NULL,
-    "Time_End" string   NOT NULL,
-    "Price_Min" string   NOT NULL,
-    "Price_Max" string   NOT NULL,
-    "Category_ID" int   NOT NULL,
-    CONSTRAINT "pk_Experiences" PRIMARY KEY (
-        "ID"
-     )
+CREATE TABLE "experiences" (
+  "id" serial NOT NULL,
+  "name" varchar,
+  "source_api_id" int,
+  "experience_api_id" varchar,
+  "description" varchar,
+  "url" varchar,
+  "img_large" varchar,
+  "img_small" varchar,
+  "venue" varchar,
+  "location" varchar,
+  "time_start" varchar,
+  "time_end" varchar,
+  "price_min" varchar,
+  "price_max" varchar,
+  "category_id" int,
+  CONSTRAINT "pk_experiences" PRIMARY KEY (
+    "id"
+   )
 );
 
-CREATE TABLE "API" (
-    "ID" int   NOT NULL,
-    "Name" string   NOT NULL,
-    CONSTRAINT "pk_API" PRIMARY KEY (
-        "ID"
-     )
+CREATE TABLE "api" (
+  "id" serial NOT NULL,
+  "name" varchar,
+  CONSTRAINT "pk_api" PRIMARY KEY (
+    "id"
+   )
 );
 
-CREATE TABLE "Categories" (
-    "ID" int   NOT NULL,
-    "Name" string   NOT NULL,
-    CONSTRAINT "pk_Categories" PRIMARY KEY (
-        "ID"
-     )
+CREATE TABLE "categories" (
+  "id" serial NOT NULL,
+  "name" varchar,
+  CONSTRAINT "pk_categories" PRIMARY KEY (
+    "id"
+   )
 );
 
-CREATE TABLE "Users_Experiences" (
-    "ID" int   NOT NULL,
-    "User_ID" int   NOT NULL,
-    "Experience_ID" int   NOT NULL,
-    CONSTRAINT "pk_Users_Experiences" PRIMARY KEY (
-        "ID"
-     )
+CREATE TABLE "users_experiences" (
+  "id" serial NOT NULL,
+  "user_id" int,
+  "experience_id" int,
+  CONSTRAINT "pk_users_experiences" PRIMARY KEY (
+    "id"
+   )
 );
 
-CREATE TABLE "Users_Categories" (
-    "ID" int   NOT NULL,
-    "User_ID" int   NOT NULL,
-    "Category_ID" int   NOT NULL,
-    CONSTRAINT "pk_Users_Categories" PRIMARY KEY (
-        "ID"
-     )
+CREATE TABLE "users_categories" (
+  "id" serial NOT NULL,
+  "user_id" int,
+  "category_id" int,
+  CONSTRAINT "pk_users_categories" PRIMARY KEY (
+    "id"
+   )
 );
 
-ALTER TABLE "Sessions" ADD CONSTRAINT "fk_Sessions_User_ID" FOREIGN KEY("User_ID")
-REFERENCES "Users" ("ID");
+ALTER TABLE "sessions" ADD CONSTRAINT "fk_sessions_user_id" FOREIGN KEY("user_id")
+REFERENCES "users" ("id");
 
-ALTER TABLE "Unvailable" ADD CONSTRAINT "fk_Unvailable_User_ID" FOREIGN KEY("User_ID")
-REFERENCES "Users" ("ID");
+ALTER TABLE "unvailable" ADD CONSTRAINT "fk_unvailable_user_id" FOREIGN KEY("user_id")
+REFERENCES "users" ("id");
 
-ALTER TABLE "Experiences" ADD CONSTRAINT "fk_Experiences_Source_API_ID" FOREIGN KEY("Source_API_ID")
-REFERENCES "API" ("ID");
+ALTER TABLE "experiences" ADD CONSTRAINT "fk_experiences_source_api_id" FOREIGN KEY("source_api_id")
+REFERENCES "api" ("id");
 
-ALTER TABLE "Experiences" ADD CONSTRAINT "fk_Experiences_Category_ID" FOREIGN KEY("Category_ID")
-REFERENCES "Categories" ("ID");
+ALTER TABLE "experiences" ADD CONSTRAINT "fk_experiences_category_id" FOREIGN KEY("category_id")
+REFERENCES "categories" ("id");
 
-ALTER TABLE "Users_Experiences" ADD CONSTRAINT "fk_Users_Experiences_User_ID" FOREIGN KEY("User_ID")
-REFERENCES "Users" ("ID");
+ALTER TABLE "users_experiences" ADD CONSTRAINT "fk_users_experiences_user_id" FOREIGN KEY("user_id")
+REFERENCES "users" ("id");
 
-ALTER TABLE "Users_Experiences" ADD CONSTRAINT "fk_Users_Experiences_Experience_ID" FOREIGN KEY("Experience_ID")
-REFERENCES "Experiences" ("ID");
+ALTER TABLE "users_experiences" ADD CONSTRAINT "fk_users_experiences_experience_id" FOREIGN KEY("experience_id")
+REFERENCES "experiences" ("id");
 
-ALTER TABLE "Users_Categories" ADD CONSTRAINT "fk_Users_Categories_User_ID" FOREIGN KEY("User_ID")
-REFERENCES "Users" ("ID");
+ALTER TABLE "users_categories" ADD CONSTRAINT "fk_users_categories_user_id" FOREIGN KEY("user_id")
+REFERENCES "users" ("id");
 
-ALTER TABLE "Users_Categories" ADD CONSTRAINT "fk_Users_Categories_Category_ID" FOREIGN KEY("Category_ID")
-REFERENCES "Categories" ("ID");
-
-CREATE INDEX "idx_Users_Name"
-ON "Users" ("Name");
+ALTER TABLE "users_categories" ADD CONSTRAINT "fk_users_categories_category_id" FOREIGN KEY("category_id")
+REFERENCES "categories" ("id");
