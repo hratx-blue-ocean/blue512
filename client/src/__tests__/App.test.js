7285268505a1fe1a1003d69e3c1f7a9958a37a5e
Landon <landon.baker@utexas.edu>
2019-08-12 17:38:24 -0500

    write render and child component tests

--- client/src/__tests__/App.test.js
+++ client/src/__tests__/App.test.js
@@ -1,20 +1,24 @@
-import { shallow } from "enzyme";
-import App from "../App";
-import React from "react";
-import NavBar from "../components/Navbar";
+import { shallow } from 'enzyme';
+import App from '../App';
+import React from 'react';
+import NavBar from '../components/Navbar';
+import MainView from '../components/MainView';
 
-describe("Top level App component", () => {
+describe('Top level App component', () => {
   let wrapper;
   beforeAll(() => {
     wrapper = shallow(<App />);
   });
-  test("it renders without crashing", () => {
-    expect(wrapper).toBeTruthy();
+  test('it renders without crashing', () => {
+    expect(wrapper).toMatchSnapshot();
   });
-  test("it has a single NavBar child", () => {
+  test('it renders a single NavBar child', () => {
     expect(wrapper.find(NavBar)).toHaveLength(1);
   });
-  test("it has the correct state shape", () => {
+  test('it renders a single MainView child', () => {
+    expect(wrapper.find(MainView)).toHaveLength(1);
+  });
+  test('it has the correct state shape', () => {
     expect(wrapper.state().eventsPlaceHolder).toBeInstanceOf(Object);
   });
 });
--- client/src/__tests__/NavBar.test.js
+++ client/src/__tests__/NavBar.test.js
@@ -1,6 +1,13 @@
 import { shallow } from 'enzyme';
 import React from 'react';
 import NavBar from '../components/Navbar';
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  InputBase,
+  Button
+} from '@material-ui/core';
 
 describe('Nav Bar component', () => {
   let wrapper;
@@ -8,6 +15,13 @@ describe('Nav Bar component', () => {
     wrapper = shallow(<NavBar />);
   });
   test('it renders without crashing', () => {
-    expect(wrapper).toBeTruthy();
+    expect(wrapper).toMatchSnapshot();
+  });
+  test('it renders the corect child components', () => {
+    expect(wrapper.find(AppBar).length).toBeGreaterThan(0);
+    expect(wrapper.find(Button).length).toBeGreaterThan(0);
+    expect(wrapper.find(InputBase).length).toBeGreaterThan(0);
+    expect(wrapper.find(Toolbar).length).toBeGreaterThan(0);
+    expect(wrapper.find(Typography).length).toBeGreaterThan(0);
   });
 });
