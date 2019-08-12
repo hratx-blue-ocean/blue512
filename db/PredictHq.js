61ee626386ba172d71769fb7969d5066b89a8386
tyler <tstendara@gmail.com>
2019-08-12 13:22:18 -0500

    setup for api

--- db/PredictHq.js
+++ db/PredictHq.js
@@ -1,5 +1,19 @@
 const axios = require('axios');
+const config = {"Authorization":"Bearer gWxFwg4Ohs4lLCQAgEr9SVsZbIQ7rC"};
+const callAPI = () => {
+    return axios.get("https://api.predicthq.com/v1/events?within=10km@30.267153,-97.7430608", {headers: config})
+    .then(res => {
+        
+    })
+}
 
-callApi() => {
-    
+const restructureData = () => {
+    let events = [];
+
+}
+
+
+
+const getData = () => {
+    return callAPI().then(data => restructureData(data))
 }
