4433ea805f64d193a44c6731af2f94007c4b13e7
Hadley Crowl <hadley.a.crowl@gmail.com>
2019-08-12 16:30:21 -0500

    Start React Router Implementation

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import fetch from 'node-fetch';
 import MainView from './components/MainView.js'
 import Navbar from './components/Navbar'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 // import './App.css';
 
 export default class App extends Component {
@@ -10,51 +11,51 @@ export default class App extends Component {
     this.state = {
       eventsPlaceHolder: [
         {
-            source_API: 'TicketMaster',
-            name: 'Hayes Carll',
-            url: 'http://www.ticketsnow.com/InventoryBrowse/TicketList.aspx?PID=2718472',
-            event_id: 'Z7r9jZ1Aejbow',
-            time_start: '2019-08-11T02:00:00Z',
-            time_end: null,
-            category: 'Music',
-            image: 'https://s1.ticketm.net/dam/a/fc1/e7affb5a-4ba1-4e6f-8aad-29c79f4a6fc1_68981_RECOMENDATION_16_9.jpg',
-            venue: 'Gruene Hall',
-            location: 'New Braunfels',
-            price_min: null,
-            price_max: null,
-            description: null
-          },
-          {
-            source_API: 'TicketMaster',
-            name: 'Trevor Cannon',
-            url: 'http://www.ticketsnow.com/InventoryBrowse/TicketList.aspx?PID=2718472',
-            event_id: 'Z7r9jZ1Aejboz',
-            time_start: '2019-08-11T02:00:00Z',
-            time_end: null,
-            category: 'Music',
-            image: 'https://s1.ticketm.net/dam/a/fc1/e7affb5a-4ba1-4e6f-8aad-29c79f4a6fc1_68981_RECOMENDATION_16_9.jpg',
-            venue: 'Gruene Hall',
-            location: 'New Braunfels',
-            price_min: null,
-            price_max: null,
-            description: null
-          },
-          {
-            source_API: 'TicketMaster',
-            name: 'Hadley Crowl',
-            url: 'http://www.ticketsnow.com/InventoryBrowse/TicketList.aspx?PID=2718472',
-            event_id: 'Z7r9jZ1Aejbot',
-            time_start: '2019-08-11T02:00:00Z',
-            time_end: null,
-            category: 'Music',
-            image: 'https://s1.ticketm.net/dam/a/fc1/e7affb5a-4ba1-4e6f-8aad-29c79f4a6fc1_68981_RECOMENDATION_16_9.jpg',
-            venue: 'Gruene Hall',
-            location: 'New Braunfels',
-            price_min: null,
-            price_max: null,
-            description: null
-          }
-    ]
+          source_API: 'TicketMaster',
+          name: 'Hayes Carll',
+          url: 'http://www.ticketsnow.com/InventoryBrowse/TicketList.aspx?PID=2718472',
+          event_id: 'Z7r9jZ1Aejbow',
+          time_start: '2019-08-11T02:00:00Z',
+          time_end: null,
+          category: 'Music',
+          image: 'https://s1.ticketm.net/dam/a/fc1/e7affb5a-4ba1-4e6f-8aad-29c79f4a6fc1_68981_RECOMENDATION_16_9.jpg',
+          venue: 'Gruene Hall',
+          location: 'New Braunfels',
+          price_min: null,
+          price_max: null,
+          description: null
+        },
+        {
+          source_API: 'TicketMaster',
+          name: 'Trevor Cannon',
+          url: 'http://www.ticketsnow.com/InventoryBrowse/TicketList.aspx?PID=2718472',
+          event_id: 'Z7r9jZ1Aejboz',
+          time_start: '2019-08-11T02:00:00Z',
+          time_end: null,
+          category: 'Music',
+          image: 'https://s1.ticketm.net/dam/a/fc1/e7affb5a-4ba1-4e6f-8aad-29c79f4a6fc1_68981_RECOMENDATION_16_9.jpg',
+          venue: 'Gruene Hall',
+          location: 'New Braunfels',
+          price_min: null,
+          price_max: null,
+          description: null
+        },
+        {
+          source_API: 'TicketMaster',
+          name: 'Hadley Crowl',
+          url: 'http://www.ticketsnow.com/InventoryBrowse/TicketList.aspx?PID=2718472',
+          event_id: 'Z7r9jZ1Aejbot',
+          time_start: '2019-08-11T02:00:00Z',
+          time_end: null,
+          category: 'Music',
+          image: 'https://s1.ticketm.net/dam/a/fc1/e7affb5a-4ba1-4e6f-8aad-29c79f4a6fc1_68981_RECOMENDATION_16_9.jpg',
+          venue: 'Gruene Hall',
+          location: 'New Braunfels',
+          price_min: null,
+          price_max: null,
+          description: null
+        }
+      ]
     };
     this.api = `http://localhost:8000/api/example`;
   }
@@ -69,13 +70,21 @@ export default class App extends Component {
 
   }
 
+  routeMainView() {
+    return <MainView events={this.state.eventsPlaceHolder} />
+  }
+
+  routeTest() {
+    return <h1>Test of alt route</h1>
+  }
+
   render() {
     return (
       <>
 
         <h1>Welcome to Blue Ocean!</h1>
         <Navbar />
-        <MainView events={this.state.eventsPlaceHolder}/>
+        <MainView events={this.state.eventsPlaceHolder} />
       </>
     );
   }
