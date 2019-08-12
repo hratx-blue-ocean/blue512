dc48c1993b5bf6efa192cefe62c7c6028063d4f7
Landon <landon.baker@utexas.edu>
2019-08-12 14:42:55 -0500

    abstract test setup into separate file

--- client/src/__tests__/App.test.js
+++ client/src/__tests__/App.test.js
@@ -1,10 +1,7 @@
-import Enzyme, { shallow } from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
+import { shallow } from "enzyme";
 import App from "../App";
 import React from "react";
 import NavBar from "../components/Navbar";
-
-Enzyme.configure({ adapter: new Adapter() });
 
 describe("Top level App component", () => {
   let wrapper;
