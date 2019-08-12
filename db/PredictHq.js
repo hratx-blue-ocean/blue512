43729e1e32a072e482797da26685357c26e1d929
tyler <tstendara@gmail.com>
2019-08-12 13:20:45 -0500

    .

--- db/PredictHq.js
+++ db/PredictHq.js
@@ -1,5 +1,23 @@
 const axios = require('axios');
-
-callApi() => {
-    
+const config = {"Authorization":"Bearer gWxFwg4Ohs4lLCQAgEr9SVsZbIQ7rC"};
+const callAPI = () => {
+    return axios.get("https://api.predicthq.com/v1/events?within=10km@30.267153,-97.7430608", {headers: config})
+    .then(res => {
+        
+    })
 }
+
+const restructureData = () => {
+    let events = [];
+
+}
+
+
+
+const getData = () => {
+    return callAPI().then(data => restructureData(data))
+}
+
+
+
+
