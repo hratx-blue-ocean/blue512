68ac5a170d83ecb483fe36565744fb7f1231a1bd
Trevor Cannon <cannon.trevor@gmail.com>
2019-08-13 15:21:18 -0500

    changed how props are passed to satisfy tests
    Co-authored-by: cnebs <38631648+cnebs@users.noreply.github.com>

--- client/src/components/MicroCardContainer.js
+++ client/src/components/MicroCardContainer.js
@@ -14,13 +14,22 @@ const useStyles = makeStyles(theme => ({
 
 export default function MicroCardContainer({ events }) {
   const classes = useStyles();
-
   return (
 
     // Build mini navbar onto container
 
     <Grid item xs={3}>
       <Grid item xs={12} className={classes.cardColumn}>
+        <MicroCardMaker event={events[0]} />
+      </Grid>
+      <Grid item xs={12} className={classes.cardColumn}>
+        <MicroCardMaker event={events[0]} />
+      </Grid>
+      <Grid item xs={12} className={classes.cardColumn}>
+        <MicroCardMaker event={events[0]} />
+      </Grid>
+      <Grid item xs={12} className={classes.cardColumn}>
+        <MicroCardMaker event={events[0]} />
       </Grid>
     </Grid>
   );
