5352f388fad7fa82c63efb155456b7f2859ddc26
chrisfauries <45318711+chrisfauries@users.noreply.github.com>
2019-08-13 17:17:17 -0500

    handling getting and inserting/updating user_categories

--- db/schema.sql
+++ db/schema.sql
@@ -108,20 +108,40 @@ REFERENCES users (id);
 ALTER TABLE users_categories ADD CONSTRAINT fk_users_categories_category_id FOREIGN KEY(category_id)
 REFERENCES categories (id);
 
-CREATE FUNCTION delete_old_unavailable()
+CREATE OR REPLACE FUNCTION delete_old_unavailable
+()
   RETURNS trigger
   LANGUAGE plpgsql
   AS $OLDUNAVAILABLE$
 BEGIN
-  DELETE FROM limiter WHERE timestamp < NOW();
+  DELETE FROM unavailable WHERE time_start < NOW();
   RETURN NULL;
 END;
-$OLDUNAVAILABLE$
+$OLDUNAVAILABLE$;
 
 
-CREATE TRIGGER trigger_delete_old_rows
+CREATE OR REPLACE TRIGGER trigger_delete_old_rows_from_unavailable
     AFTER
 INSERT ON
-limiter
-EXECUTE PROCEDURE delete_old_rows
+unavailable
+EXECUTE PROCEDURE delete_old_unavailable
+();
+
+
+CREATE OR REPLACE FUNCTION delete_old_experiences
+()
+  RETURNS trigger
+  LANGUAGE plpgsql
+  AS $OLDEXPERIENCES$
+BEGIN
+  DELETE FROM experiences WHERE time_start < NOW();
+  RETURN NULL;
+END;
+$OLDEXPERIENCES$;
+
+CREATE TRIGGER trigger_delete_old_rows_from_unavailable
+    AFTER
+INSERT ON
+experiences
+EXECUTE PROCEDURE delete_old_experiences
 ();
