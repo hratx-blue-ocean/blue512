62eb059ecdf0f173cd618f3e01ba750b86c68ce4
Landon <landon.baker@utexas.edu>
2019-08-13 20:01:25 -0500

    load token

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,8 +1,8 @@
-import React from "react";
-import { fade, makeStyles } from "@material-ui/core/styles";
-import SearchIcon from "@material-ui/icons/Search";
-import { Link as RouterLink } from "react-router-dom";
-import axios from "axios";
+import React from 'react';
+import { fade, makeStyles } from '@material-ui/core/styles';
+import SearchIcon from '@material-ui/icons/Search';
+import { Link as RouterLink } from 'react-router-dom';
+import axios from 'axios';
 import {
   MenuItem,
   AppBar,
@@ -11,77 +11,77 @@ import {
   InputBase,
   Button,
   IconButton
-} from "@material-ui/core/";
-import SettingsIcon from "@material-ui/icons/Settings";
+} from '@material-ui/core/';
+import SettingsIcon from '@material-ui/icons/Settings';
 
 const useStyles = makeStyles(theme => ({
   grow: {
     flexGrow: 1
   },
   title: {
-    display: "none",
-    [theme.breakpoints.up("sm")]: {
-      display: "block"
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block'
     }
   },
   button: {
     margin: theme.spacing(1),
-    color: "white",
+    color: 'white',
     fontSize: 13
   },
   Signupbutton: {
     margin: theme.spacing(1),
-    color: "white",
+    color: 'white',
     fontSize: 18
   },
   input: {
-    display: "none"
+    display: 'none'
   },
   search: {
-    position: "relative",
+    position: 'relative',
     borderRadius: theme.shape.borderRadius,
     backgroundColor: fade(theme.palette.common.white, 0.15),
-    "&:hover": {
+    '&:hover': {
       backgroundColor: fade(theme.palette.common.white, 0.25)
     },
     marginRight: theme.spacing(2),
     marginLeft: 0,
-    width: "100%",
-    [theme.breakpoints.up("sm")]: {
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
       marginLeft: theme.spacing(3),
-      width: "auto"
+      width: 'auto'
     }
   },
   searchIcon: {
     width: theme.spacing(7),
-    height: "100%",
-    position: "absolute",
-    pointerEvents: "none",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center"
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
   },
   inputRoot: {
-    color: "inherit"
+    color: 'inherit'
   },
   inputInput: {
     padding: theme.spacing(1, 1, 1, 7),
-    transition: theme.transitions.create("width"),
-    width: "100%",
-    [theme.breakpoints.up("md")]: {
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
       width: 200
     }
   },
   sectionDesktop: {
-    display: "none",
-    [theme.breakpoints.up("md")]: {
-      display: "flex"
+    display: 'none',
+    [theme.breakpoints.up('md')]: {
+      display: 'flex'
     }
   },
   sectionMobile: {
-    display: "flex",
-    [theme.breakpoints.up("md")]: {
-      display: "none"
+    display: 'flex',
+    [theme.breakpoints.up('md')]: {
+      display: 'none'
     }
   }
 }));
@@ -93,9 +93,7 @@ export default function PrimarySearchAppBar(props) {
     console.log(id_token);
     axios
       .post(
-        `http://ec2-52-15-83-226.us-east-2.compute.amazonaws.com:${
-          props.port
-        }/api/events`,
+        `http://ec2-52-15-83-226.us-east-2.compute.amazonaws.com:${props.port}/api/events`,
         {
           token: id_token,
           calendar_items,
@@ -103,7 +101,7 @@ export default function PrimarySearchAppBar(props) {
           day: null
         }
       )
-      .then(res => props.loadEvents(res.data))
+      .then(res => props.loadEvents({ id_token, ...res.data }))
       .catch();
   };
 
@@ -115,12 +113,12 @@ export default function PrimarySearchAppBar(props) {
   window.getCalData = id_token => {
     window.gapi.client.calendar.events
       .list({
-        calendarId: "primary",
+        calendarId: 'primary',
         timeMin: new Date().toISOString(),
         showDeleted: false,
         singleEvents: true,
         maxResults: 10,
-        orderBy: "startTime"
+        orderBy: 'startTime'
       })
       .then(data => {
         getData(id_token, data.result.items);
@@ -131,9 +129,7 @@ export default function PrimarySearchAppBar(props) {
     <div className={classes.grow}>
       <AppBar position="static">
         <Toolbar>
-          <Typography className={classes.title} >
-            GoDo
-          </Typography>
+          <Typography className={classes.title}>GoDo</Typography>
           <div className={classes.search}>
             <div className={classes.searchIcon}>
               <SearchIcon />
@@ -144,7 +140,7 @@ export default function PrimarySearchAppBar(props) {
                 root: classes.inputRoot,
                 input: classes.inputInput
               }}
-              inputProps={{ "aria-label": "search" }}
+              inputProps={{ 'aria-label': 'search' }}
             />
           </div>
           <Button component={RouterLink} to="/" className={classes.button}>
