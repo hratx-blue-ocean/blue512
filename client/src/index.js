d944958815e4fa64f37ab30cc9035f5a4a51bf31
Hadley Crowl <hadley.a.crowl@gmail.com>
2019-08-14 15:41:47 -0500

    Add theme provider and theme object

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.js';
-// import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
+import blue from '@material-ui/core/colors/blue';
+
+//Set global theme here
+const theme = createMuiTheme({
+  palette: {
+    primary: blue,
+  },
+});
+
+ReactDOM.render(
+  <MuiThemeProvider theme={theme}>
+    <App />
+  </MuiThemeProvider>
+  , document.getElementById('root'));
