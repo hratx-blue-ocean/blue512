4c8c0347f92082593618d9ceda7cafdb327c8e02
Hadley Crowl <hadley.a.crowl@gmail.com>
2019-08-14 16:09:23 -0500

    change primary color to red

--- client/src/index.js
+++ client/src/index.js
@@ -3,17 +3,31 @@ import ReactDOM from 'react-dom';
 import App from './App.js';
 
 import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
-import blue from '@material-ui/core/colors/blue';
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { yellow, red, blue } from '@material-ui/core/colors';
 
 //Set global theme here
 const theme = createMuiTheme({
   palette: {
-    primary: blue,
+    // primary: yellow
+
+    primary: {
+      // light: "#7986cb",
+      main: "#ff1744",
+      // dark: "#303f9f",
+    },
+    background: {
+      paper: "#fff",
+      default: "#fafafa"
+      // default: "#ffde03"
+    }
   },
 });
 
+
 ReactDOM.render(
   <MuiThemeProvider theme={theme}>
+    <CssBaseline />
     <App />
   </MuiThemeProvider>
   , document.getElementById('root'));
