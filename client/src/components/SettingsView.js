a8590d2db15b303502ae2a3d70625a3536a68073
Landon <landon.baker@utexas.edu>
2019-08-14 17:18:29 -0500

    pull in user name and avatar

--- client/src/components/SettingsView.js
+++ client/src/components/SettingsView.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import PreferencesContainer from './PreferencesContainer';
-import { Grid, Container } from '@material-ui/core/';
+import { Grid, Typography, Avatar } from '@material-ui/core/';
 
 export default class SettingsView extends React.Component {
   constructor(props) {
@@ -36,16 +36,35 @@ export default class SettingsView extends React.Component {
   render() {
     return (
       <>
-        <Container maxWidth="lg">
-          <Grid container>
+        <Grid container spacing={1} justify="center" alignItems="center">
+          <Grid item>
+            <Avatar
+              align="center"
+              alt="User Avatar"
+              src={this.props.user.avatar_url}
+              style={{ marginTop: 10, width: 40, height: 40 }}
+            />
+          </Grid>
+          <Grid item>
+            <Typography variant="h4" style={{ marginTop: 10 }} align="center">
+              Hello {this.props.user.first_name}!
+            </Typography>
+          </Grid>
+          <Grid item xs={12} justify="center">
+            <Typography variant="subtitle1" align="center">
+              Help us custom tailor your CityScout experience!
+            </Typography>
+          </Grid>
+          <Grid item xs={10} justify="center" align="center">
             <PreferencesContainer
               categories={this.state.categories}
               userPreferences={this.state.userPreferences}
               handleChange={this.postNewPreference}
               userToken={this.props.userToken}
+              style={{ align: 'center' }}
             />
           </Grid>
-        </Container>
+        </Grid>
       </>
     );
   }
