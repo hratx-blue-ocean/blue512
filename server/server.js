a98fb4edab23222342ee90ea376b3ca553c73a27
Gary Blaser <glblaser@gmail.com>
2019-08-15 14:01:53 -0500

    Added cronjobs to server and start them

--- server/server.js
+++ server/server.js
@@ -1,13 +1,18 @@
-require("dotenv").config();
-const createError = require("http-errors");
-const logger = require("morgan");
-const express = require("express");
+require('dotenv').config();
+const createError = require('http-errors');
+const logger = require('morgan');
+const express = require('express');
 const app = express();
-const query = require("../db/query.js");
-const cronOperations = require("../db/cron.js");
-const path = require("path");
-const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
+const query = require('../db/query.js');
+const cronOperations = require('../db/cron.js');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const { atMidnightEveryDay, everyTenMinutes } = require('../db/cron.js');
+
+//start cronjobs
+atMidnightEveryDay.start();
+everyTenMinutes.start();
 
 app.use(bodyParser.json());
 app.use(cookieParser());
@@ -37,34 +42,34 @@ xapp.get('/someOtherRoute', async(req, res) => {
 
 // open up CORS
 app.use((_, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
 });
 
-app.use(logger("dev"));
-app.use("/", express.static(path.join(__dirname, "../client/public")));
+app.use(logger('dev'));
+app.use('/', express.static(path.join(__dirname, '../client/public')));
 // You can place your routes here, feel free to refactor:
-const { api } = require("./routes");
-app.use("/api/", api);
+const { api } = require('./routes');
+app.use('/api/', api);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    next(createError(404));
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
 // error handler
-app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get("env") === "development" ? err : {};
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render("error");
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 module.exports = app;
