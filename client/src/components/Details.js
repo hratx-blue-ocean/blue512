a39cd0dab10ebeb2f8eb64a3fe25a8a9c01a5664
Justan Other Human <iamjustanotherhuman@gmail.com>
2019-08-14 19:41:19 -0500

    added material detail view

--- client/src/components/Details.js
+++ client/src/components/Details.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Grid } from '@material-ui/core/';
+import { Grid, Card } from '@material-ui/core/';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import Button from '@material-ui/core/Button';
 
 const useStyles = makeStyles(theme => ({
   root: {
     padding: theme.spacing(3, 2),
   },
+
 }));
 
 export default function Details({ event }) {
@@ -17,12 +20,25 @@ export default function Details({ event }) {
     <Grid item xs={8}>
       <div>
         <Paper className={classes.root}>
-          <Typography variant='h5' component='h3'>
-            This is a sheet of paper.
-        </Typography>
-          <Typography component='p'>
-            Paper can be used to build surface or other elements for your application.
-        </Typography>
+          <Typography variant='h3' component='h2'>
+            Today: (hardcoded) {event.name}
+          </Typography>
+          <img src={event.img} style={{ width: '100%' }}/>
+          <Typography variant='h5' component='h5'>
+            Description: (currently null for this event){event.description}
+           </Typography>
+           <Typography variant='h5' component='h5'>
+              At: {event.time_start} to {event.time_end}
+           </Typography>
+           <Typography variant='h5' component='h5'>
+             Venue: {event.venue} 
+           </Typography>
+           <Typography variant='h5' component='p'>
+             <ButtonGroup>
+              <Button>Add to calender</Button>
+              <Button>Get more info *url*</Button>
+             </ButtonGroup>
+           </Typography>
         </Paper>
       </div>
     </Grid>
