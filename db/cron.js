08b9618cc32eee9e7a8a5062ccf2ac4479e168d0
chrisfauries <45318711+chrisfauries@users.noreply.github.com>
2019-08-15 15:14:27 -0500

    cron fix

--- db/cron.js
+++ db/cron.js
@@ -54,7 +54,7 @@ const atMidnightEveryDay = new CronJob('0 0 0 * * *', () => {
     .catch(console.log);
 });
 
-const everyTenMinutes = new CronJob('*/10 * * * * *', () => {
+const everyTenMinutes = new CronJob('0 */10 * * * *', () => {
   deleteOldExperiences()
     .then(console.log)
     .catch(console.log);
